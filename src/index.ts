185b6aa6fdbd18bd3973d4e6493f4707ffec9bb9
Baseer Afridi <githubbs7@gmail.com>
2026-04-09 15:48:19 +0500

    fixes: ts eror fixed

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = +(process.env.PORT ?? 3000)
 
 app.get('/', (_req, res) => {
   res.send('<h1> hello it work v1 </h1>');
 });
 
-app.listen(port, () => {
-  console.log({port})
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
+  console.log(`Public IP should be accessible at ${process.env.PUBLIC_IP || 'localhost'}:${port} `);
 });
