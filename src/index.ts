d4018023a914fffd0963b800bab3cd5ef3fdfc69
Baseer Afridi <githubbs7@gmail.com>
2026-04-09 16:29:30 +0500

    check: ci cd check

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = +(process.env.PORT ?? 3000)
 
 app.get('/', (_req, res) => {
-  res.send('<h1> hello it work v2 with CI/CD </h1>');
+  res.send('<h1> hello it work v3 with CI/CD </h1>');
 });
 
 app.listen(port, '0.0.0.0', () => {
